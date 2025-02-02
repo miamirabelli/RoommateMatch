994c0f2037993c7c3fe0ca60fd34d78699939d9a
Mia Mirabelli <miamirabelli@gmail.com>
2025-02-01 16:56:30 -0800

    made pd dataframe

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+columns = ['name', 'age', 'gender', 'budget', 'area', 'roommates_needed', 'cleanliness', 'bedtime', 'friends_over', 'friend_level', 'pets', 'loud', 'most_important']
+df_users = pd.DataFrame(columns=columns)
+global df_users
 
-users_data = []
+# this is the main route -- aka, it displays the form initially
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/match")
+def parse_form():
+    if request.method == "POST":
+
+
+
 
